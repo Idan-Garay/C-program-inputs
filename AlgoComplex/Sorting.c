8d791a2c4cde052def0d021e94a99239451d0047
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2021-09-20 15:00:41 +0800

    added shell, quick, bucket, merge sort

---
+++ AlgoComplex/Sorting.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "SortingAlgorithms.c"
+
+int main()
+{
+  int A[] = {4, 3, 2, 10, 12, 1, 5, 6};
+  float B[] = {0.4, 0.3521, 0.224, 0.10, 0.12, 0.1, 0.5, 0.6};
+  int C[] = {231, 0, 76, 5, 10005, 6044, 7012, 3988};
+  // testShellSort(A, 8);
+  // testQuickSort(A, 8);
+  // testBucketSort(B, 8);
+  testCountingSort(A, 8);
+  // testRadixSort(C, 8);
+
+  return 0;
+}
---
+++ AlgoComplex/SortingAlgorithms.c
@@ -0,0 +1,296 @@
+void display(int arr[], int size);
+void displayF(float arr[], int size);
+
+// shell Sort
+void shellSort(int A[], int size)
+{
+  int gap, x, y, temp;
+
+  for (gap = size / 2; gap > 0; gap /= 2)
+  {
+    for (x = gap; x < size; x += gap)
+    {
+      temp = A[x];
+      for (y = x; y > 0 && temp < A[y - gap]; y -= gap)
+      {
+        A[y] = A[y - gap];
+      }
+      A[y] = temp;
+    }
+  }
+}
+
+void testShellSort(int A[], int size)
+{
+  printf("Shell Sort: \n");
+  display(A, size);
+  shellSort(A, size);
+  printf(" -> ");
+  display(A, size);
+  printf("\n");
+}
+
+// Merge Sort
+void merge(int A[], int lo, int mid, int hi)
+{
+  int leftLength = mid - lo + 1;
+  int rightLength = hi - mid;
+  int left[leftLength];
+  int rightPart[rightLength];
+  int j, g, k;
+
+  for (g = 0; g < leftLength; g++)
+  {
+    left[g] = A[lo + g - 1];
+  }
+
+  for (j = 0; j < rightLength; j++)
+  {
+    rightPart[j] = A[mid + 1 + j];
+  }
+
+  j = g = 0;
+  for (k = lo; k <= hi; k++)
+  {
+    if (j < leftLength && left[j] <= rightPart[g])
+    {
+      A[k] = left[j++];
+    }
+    else
+    {
+      A[k] = rightPart[g++];
+    }
+  }
+}
+
+void mergeSort(int A[], int lo, int hi)
+{
+  if (lo < hi)
+  {
+    int mid = (hi + lo) / 2;
+    mergeSort(A, lo, mid);
+    mergeSort(A, mid + 1, hi);
+    merge(A, lo, mid, hi);
+  }
+}
+
+void testMergeSort(int A[], int size)
+{
+  printf("Merge Sort: \n");
+  display(A, size);
+  mergeSort(A, 0, size - 1);
+  printf(" -> ");
+  display(A, size);
+  printf("\n");
+}
+
+// quick sort
+
+int partition(int A[], int lo, int hi)
+{
+  int pivot = A[hi];
+
+  int i, j, swap;
+
+  i = lo - 1;
+  for (j = lo; j < hi; j++)
+  {
+    if (A[j] < pivot)
+    {
+      i++;
+      swap = A[j];
+      A[j] = A[i];
+      A[i] = swap;
+    }
+  }
+  swap = A[i + 1];
+  A[i + 1] = A[hi];
+  A[hi] = swap;
+
+  return i + 1;
+}
+
+void quickSort(int A[], int lo, int hi)
+{
+  if (lo < hi)
+  {
+    int pivot = partition(A, lo, hi);
+
+    quickSort(A, lo, pivot - 1);
+    quickSort(A, pivot + 1, hi);
+  }
+}
+
+void testQuickSort(int A[], int size)
+{
+  printf("Quick Sort: \n");
+  display(A, size);
+  quickSort(A, 0, size - 1);
+  printf(" -> ");
+  display(A, size);
+  printf("\n");
+}
+
+typedef struct node
+{
+  float val;
+  struct node *next;
+} node, *ptr;
+
+// Bucket Sort
+#include <stdlib.h>
+void insertionSort(ptr *List);
+void displayBuckets(ptr A[], int size)
+{
+  int x;
+  ptr trav;
+
+  printf("\n");
+  for (x = 0; x < size; x++)
+  {
+    printf("[%d]: ", x);
+    for (trav = A[x]; trav != NULL; trav = trav->next)
+    {
+      printf(" %f ->", trav->val);
+    }
+    printf("\n");
+  }
+}
+
+void bucketSort(float A[], int size)
+{
+  ptr trav, *buckets = calloc(size, sizeof(ptr));
+  int x, k, idx;
+  float _MAX = A[0];
+
+  for (x = 1; x < size; x++)
+  {
+    if (_MAX < A[x])
+      _MAX = A[x];
+  }
+
+  for (x = 0; x < size; x++)
+  {
+    idx = (A[x] * size);
+    ptr element = malloc(sizeof(node));
+    element->val = A[x];
+    element->next = buckets[idx];
+    buckets[idx] = element;
+  }
+
+  k = 0;
+  for (x = 0; x < size; x++)
+  {
+    insertionSort(buckets + x);
+    for (trav = buckets[x]; k < size && trav != NULL; trav = trav->next)
+    {
+      A[k++] = trav->val;
+    }
+  }
+
+  free(buckets);
+}
+
+void testBucketSort(float A[], int size)
+{
+
+  printf("Bucket Sort: \n");
+  displayF(A, size);
+  bucketSort(A, size);
+  printf(" -> ");
+  displayF(A, size);
+  printf("\n");
+}
+
+void insertionSort(ptr *List)
+{
+
+  if (*List != NULL && (*List)->next != NULL)
+  {
+    ptr trav, trav2;
+    float tmp, hold;
+    int flag = 0;
+    for (trav = (*List)->next; trav != NULL; trav = trav->next)
+    {
+      tmp = trav->val;
+      for (trav2 = *List; trav2 != NULL; trav2 = trav2->next)
+      {
+        if (tmp < trav2->val)
+        {
+          flag = 1;
+        }
+        if (flag == 1)
+        {
+          hold = trav2->val;
+          trav2->val = tmp;
+          tmp = hold;
+        }
+      }
+    }
+  }
+}
+
+// counting sort
+#include <string.h>
+void countingSort(int A[], int size)
+{
+  int *count, x;
+  int B[size];
+  int _MAX = A[0];
+
+  memcpy(B, A, sizeof(int) * size);
+  for (x = 1; x < size; x++)
+  {
+    if (A[x] > _MAX)
+      _MAX = A[x];
+  }
+
+  count = (int *)calloc(_MAX + 1, sizeof(int));
+  for (x = 0; x < size; x++)
+  {
+    count[A[x]]++;
+  }
+
+  for (x = 1; x <= _MAX; x++)
+  {
+    count[x] += count[x - 1];
+  }
+
+  for (x = size - 1; x >= 0; x--)
+  {
+    A[--count[B[x]]] = B[x];
+  }
+  free(count);
+}
+
+void testCountingSort(int A[], int size)
+{
+
+  printf("Counting Sort: \n");
+  display(A, size);
+  countingSort(A, size);
+  printf(" -> ");
+  display(A, size);
+  printf("\n");
+}
+
+// display utility
+void display(int arr[], int size)
+{
+  int x;
+
+  for (x = 0; x < size; x++)
+  {
+    printf(" %d", arr[x]);
+  }
+}
+
+void displayF(float arr[], int size)
+{
+  int x;
+
+  for (x = 0; x < size; x++)
+  {
+    printf(" %g", arr[x]);
+  }
+}
